35a570da93e1ce44d0358d51d9cceb3dae4a9110
Oleh Omelchenko <ptrvtch+github@protonmail.com>
2021-10-31 19:55:35 +0200

    update notion formatting

--- notion_utils.py
+++ notion_utils.py
@@ -11,27 +11,43 @@ logger = logging.getLogger(__name__)
 
 def create_page(token, db_id, message):
     create_page_url = "https://api.notion.com/v1/pages/"
+    name = ""
+    if message.forward_from:
+        name = message.forward_from.first_name
+        last_name = message.forward_from.last_name
+        username = message.forward_from.username
+        username = f" ({username})" if username else ""
+        name = f"Forwarded from {name} {last_name}" + username
 
+    if message.forward_from_chat:
+        chat_title = message.forward_from_chat.title
+        chat_username = message.forward_from_chat.username
 
+        name = f"Forwarded from {chat_title} ({chat_username})"
 
-    forwarded_from_messageid = message.forward_from_message_id
-    forwarded_from_channelid = message.forward_from_chat.username
-    url = f"https://t.me/{forwarded_from_channelid}/{forwarded_from_messageid}"
+        forwarded_from_messageid = message.forward_from_message_id
+        forwarded_from_channelid = message.forward_from_chat.username
+        url = f"https://t.me/{forwarded_from_channelid}/{forwarded_from_messageid}"
+    else:
+        url = None
 
-    content = yaml.dump(message.to_dict(), allow_unicode=True)
+    message_dict = message.to_dict()
+    if message_dict.get("text"):
+        message_dict.pop("text")
+    text = message.text_markdown_urled
 
-    text = message.text
+    content = yaml.dump(message_dict, allow_unicode=True)
 
     create_page_data = {
         "parent": {"database_id": db_id},
         "properties": {
-            "Name": {"title": [{"type": "text", "text": {"content": url or ""}}]},
+            "Name": {"title": [{"type": "text", "text": {"content": name}}]},
             "Tags": {
                 "multi_select": [
                     {"name": "Telegram"},
                 ]
             },
-            "url": {"url": url or ""},
+            "url": {"url": url or "https://example.com"},
         },
         "children": [
             {
